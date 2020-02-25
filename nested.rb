f0e7cfc0cb7af48f6260781bea7bfa3443f2556c
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-25 02:46:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,7 +6,7 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        programmer_hash[]
+        programmer_hash[:grace_hopper]
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
